8b9b4910802f5a0a7097c7eb6a886a57a3d8ec09
Dillion Fox <11foxd1@gmail.com>
2022-07-11 21:30:18 -0400

    renaming files so they make more sense

--- graphox/rgcn/rgcn.py
+++ graphox/rgcn/rgcn.py
@@ -1,98 +1,64 @@
-from abc import ABC
+from datetime import datetime
 
-import numpy as np
-import pandas as pd
 import torch
-from torch_geometric.nn import MessagePassing
-from torch_scatter import scatter_add
-from torch.nn import Linear
-from torch_geometric.nn import global_mean_pool
+from graphox.rgcn.data.immotion.immotion_dataset import ImMotionDataset
+from graphox.rgcn.src import CurvatureGraph, CurvatureValues
+from torch_geometric.loader import DataLoader
+from ignite.metrics import ConfusionMatrix
 
 
-class CurvatureValues(object):
-    def __init__(self, num_nodes, ricci_filename):
-        self.num_nodes = num_nodes
-        self.ricci_filename = ricci_filename
-        self.w_mul = None
-        self.compute_w_mul()
-
-    def compute_w_mul(self):
-        df = pd.read_csv(self.ricci_filename, header=None, names=[0, 1, 2])
-        w_mul = np.array(df.sort_values(by=[0, 1])[2].tolist())
-        w_mul += np.float(abs(min(w_mul)))
-        self.w_mul = torch.from_numpy(w_mul)
+def train(dataset, model, optimizer, loss_function):
+    model.train()
+    for data in dataset:
+        pred = model(data)
+        loss = loss_function(pred, data.y.type(torch.LongTensor))
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+    return model, optimizer
 
 
-class CurvatureGraph(object):
-    def __init__(self, G, curvature_values: CurvatureValues, num_classes=2):
-        self.G = G
-        self.num_classes = num_classes
-        self.curvature_values = curvature_values
-
-    def call(self):
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        w_mul = self.compute_convolution_weights(self.G.edge_index, self.curvature_values)
-        model = CurvatureGraphNN(self.G.num_features, self.num_classes, w_mul, d_hidden=64, p=0.5)
-        model.to(device).reset_parameters()
-        return device, model
-
-    @staticmethod
-    def compute_convolution_weights(edge_index, edge_weight):
-        deg_inv_sqrt = scatter_add(edge_weight, edge_index[0], dim=0).pow(-0.5)
-        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
-        w_mul = deg_inv_sqrt[edge_index[0]] * edge_weight * deg_inv_sqrt[edge_index[1]]
-        return torch.tensor(w_mul.clone().detach(), dtype=torch.float)
+def test(dataset, model):
+    correct = 0
+    metric = ConfusionMatrix(num_classes=2)
+    for data in dataset:  # Iterate in batches over the training/test dataset.
+        out = model(data)
+        pred = out.max(1)[1]
+        correct += int((pred == data.y).sum())  # Check against ground-truth labels.
+    return correct / len(dataset.dataset)  # Derive ratio of correct predictions.
 
 
-class CurvatureGraphNN(torch.nn.Module):
-    def __init__(self, num_features, num_classes, w_mul, d_hidden=64, p=0.0):
-        super(CurvatureGraphNN, self).__init__()
-        self.conv1 = MessagePassingConvLayer(num_features, d_hidden, w_mul, p=p)
-        self.conv2 = MessagePassingConvLayer(d_hidden, d_hidden, w_mul, p=p)
-        self.lin = Linear(d_hidden, num_classes)
-
-    def reset_parameters(self):
-        self.conv1.reset_parameters()
-        self.conv2.reset_parameters()
-
-    def forward(self, data):
-        # x = torch.nn.functional.dropout(data.x, p=0.6, training=self.training)
-
-        # 1. Obtain node embeddings
-        x = self.conv1(data.x, data.edge_index)
-        x = x.relu()
-        x = self.conv2(x, data.edge_index)
-        # x = x.relu()
-
-        # 2. Readout layer
-        x = global_mean_pool(x, data.batch)  # [batch_size, d_hidden]
-
-        # 3. Apply a final classifier
-        x = torch.nn.functional.dropout(x, p=0.6, training=self.training)
-        x = self.lin(x)
-        x = torch.nn.functional.log_softmax(x, dim=1)
-
-        return x
+def main(data_path, num_trials, ricci_filename='/Users/dfox/code/graphox/data/immotion_edge_curvatures_formatted.csv'):
+    data_raw = ImMotionDataset(data_path)
+    train_dataset = data_raw[:20]
+    test_dataset = data_raw[20:30]
+    train_data = DataLoader(train_dataset, batch_size=1, shuffle=True)
+    test_data = DataLoader(test_dataset, batch_size=1, shuffle=False)
+    curvature_values = CurvatureValues(data_raw[0].num_nodes, ricci_filename=ricci_filename).w_mul
+    print('Trial, Epoch, Train acc, Test acc, Time')
+    for i in range(num_trials):
+        curvature_graph_obj = CurvatureGraph(data_raw[0], curvature_values)
+        device, model = curvature_graph_obj.call()
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+        loss_function = torch.nn.functional.nll_loss()
+        for epoch in range(20):
+            t_initial = datetime.now()
+            model, optimizer = train(train_data, model, optimizer, loss_function)
+            train_acc = test(train_data, model)
+            test_acc = test(test_data, model)
+            t_final = datetime.now()
+            print(i, epoch, train_acc, test_acc, t_final - t_initial)
 
 
-class MessagePassingConvLayer(MessagePassing, ABC):
-    def __init__(self, in_channels, out_channels, weight_agg, p=0.6):
-        super(MessagePassingConvLayer, self).__init__(aggr='add')
-        self.dropout = p
-        self.out_channels = out_channels
-        self.weight_agg = weight_agg
-        self.lin = torch.nn.Linear(in_channels, out_channels)
+if __name__ == '__main__':
+    import sys
+    import os
 
-    def reset_parameters(self):
-        self.lin.reset_parameters()
+    number_of_trials = 2
+    graphs_path = sys.argv[1]
+    ricci_path = sys.argv[2]
 
-    def forward(self, x, edge_index):
-        x = self.lin(x)
-        return self.propagate(edge_index, x=x)
+    if not os.path.exists(graphs_path):
+        print('invalid path')
 
-    def message(self, x_j):
-        weight_agg = torch.nn.functional.dropout(self.weight_agg, p=self.dropout, training=self.training)
-        return weight_agg.view(-1, 1) * x_j
-
-    def update(self, aggr_out):
-        return aggr_out
+    main(graphs_path, number_of_trials, ricci_path)
---
+++ graphox/rgcn/src.py
@@ -0,0 +1,98 @@
+from abc import ABC
+
+import numpy as np
+import pandas as pd
+import torch
+from torch_geometric.nn import MessagePassing
+from torch_scatter import scatter_add
+from torch.nn import Linear
+from torch_geometric.nn import global_mean_pool
+
+
+class CurvatureValues(object):
+    def __init__(self, num_nodes, ricci_filename):
+        self.num_nodes = num_nodes
+        self.ricci_filename = ricci_filename
+        self.w_mul = None
+        self.compute_w_mul()
+
+    def compute_w_mul(self):
+        df = pd.read_csv(self.ricci_filename, header=None, names=[0, 1, 2])
+        w_mul = np.array(df.sort_values(by=[0, 1])[2].tolist())
+        w_mul += np.float(abs(min(w_mul)))
+        self.w_mul = torch.from_numpy(w_mul)
+
+
+class CurvatureGraph(object):
+    def __init__(self, G, curvature_values: CurvatureValues, num_classes=2):
+        self.G = G
+        self.num_classes = num_classes
+        self.curvature_values = curvature_values
+
+    def call(self):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        w_mul = self.compute_convolution_weights(self.G.edge_index, self.curvature_values)
+        model = CurvatureGraphNN(self.G.num_features, self.num_classes, w_mul, d_hidden=64, p=0.5)
+        model.to(device).reset_parameters()
+        return device, model
+
+    @staticmethod
+    def compute_convolution_weights(edge_index, edge_weight):
+        deg_inv_sqrt = scatter_add(edge_weight, edge_index[0], dim=0).pow(-0.5)
+        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+        w_mul = deg_inv_sqrt[edge_index[0]] * edge_weight * deg_inv_sqrt[edge_index[1]]
+        return torch.tensor(w_mul.clone().detach(), dtype=torch.float)
+
+
+class CurvatureGraphNN(torch.nn.Module):
+    def __init__(self, num_features, num_classes, w_mul, d_hidden=64, p=0.0):
+        super(CurvatureGraphNN, self).__init__()
+        self.conv1 = MessagePassingConvLayer(num_features, d_hidden, w_mul, p=p)
+        self.conv2 = MessagePassingConvLayer(d_hidden, d_hidden, w_mul, p=p)
+        self.lin = Linear(d_hidden, num_classes)
+
+    def reset_parameters(self):
+        self.conv1.reset_parameters()
+        self.conv2.reset_parameters()
+
+    def forward(self, data):
+        # x = torch.nn.functional.dropout(data.x, p=0.6, training=self.training)
+
+        # 1. Obtain node embeddings
+        x = self.conv1(data.x, data.edge_index)
+        x = x.relu()
+        x = self.conv2(x, data.edge_index)
+        # x = x.relu()
+
+        # 2. Readout layer
+        x = global_mean_pool(x, data.batch)  # [batch_size, d_hidden]
+
+        # 3. Apply a final classifier
+        x = torch.nn.functional.dropout(x, p=0.6, training=self.training)
+        x = self.lin(x)
+        x = torch.nn.functional.log_softmax(x, dim=1)
+
+        return x
+
+
+class MessagePassingConvLayer(MessagePassing, ABC):
+    def __init__(self, in_channels, out_channels, weight_agg, p=0.6):
+        super(MessagePassingConvLayer, self).__init__(aggr='add')
+        self.dropout = p
+        self.out_channels = out_channels
+        self.weight_agg = weight_agg
+        self.lin = torch.nn.Linear(in_channels, out_channels)
+
+    def reset_parameters(self):
+        self.lin.reset_parameters()
+
+    def forward(self, x, edge_index):
+        x = self.lin(x)
+        return self.propagate(edge_index, x=x)
+
+    def message(self, x_j):
+        weight_agg = torch.nn.functional.dropout(self.weight_agg, p=self.dropout, training=self.training)
+        return weight_agg.view(-1, 1) * x_j
+
+    def update(self, aggr_out):
+        return aggr_out
