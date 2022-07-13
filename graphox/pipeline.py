5be37bc3421f5c5937d7454604d7613bc322330f
Dillion Fox <11foxd1@gmail.com>
2022-07-12 21:40:11 -0400

    reworking paths

--- graphox/pipeline.py
+++ graphox/pipeline.py
@@ -1,4 +1,13 @@
-from graphox.builder.graph_builder import GraphBuilder
+from graphox.builder.graph_builder import BaseGraphBuilder
 from graphox.graph_curvature.curvature import GraphCurvature
 from graphox.rgcn.rgcn import main
+import os
 
+root_dir = 'data/raw/'
+omics_data_ = os.path.join(root_dir, 'full_data_expr_G.csv')
+omics_anno_ = os.path.join(root_dir, 'full_data_anno.csv')
+string_aliases_file_ = os.path.join(root_dir, '9606.protein.aliases.v11.5.txt')
+string_edges_file_ = os.path.join(root_dir, '9606.protein.links.v11.5.txt')
+
+builder = BaseGraphBuilder(omics_data_, omics_anno_, string_aliases_file_, string_edges_file_)
+builder.execute()
